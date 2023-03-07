cfda5b2481f836651fb1d0eeed69590cb637def1
Oscar Rolón <oscarpcpi2gmai@gmail.com>
2023-03-07 18:27:52 +0100

    creado menu diario y añadido fondo y titulo y funcion de dia

--- src/app/components/menu-diario/menu-diario.component.ts
+++ src/app/components/menu-diario/menu-diario.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'menu-diario',
   templateUrl: './menu-diario.component.html',
   styleUrls: ['./menu-diario.component.css']
 })
-export class MenuDiarioComponent {
+export class MenuDiarioComponent implements OnInit {
+
+  dia: Date
+  semana: Object
+  diaActual: String
+  diaActualUpper: string
+  constructor() {
+    this.dia = new Date()
+    this.semana = { weekday: 'long' }
+    this.diaActual = this.dia.toLocaleDateString('es-ES', this.semana)
+    this.diaActualUpper = this.diaActual.toUpperCase()
+  }
+
+  ngOnInit() {
+
+
+  }
 
 }
