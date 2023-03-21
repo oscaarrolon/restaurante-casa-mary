ef55adb4a8b44816eaa5034ecc434abb977a9e79
Oscar Rolón <oscarpcpi2gmai@gmail.com>
2023-03-21 16:40:28 +0100

    Falta responsive para ipad air

--- src/app/components/about-us/about-us.component.ts
+++ src/app/components/about-us/about-us.component.ts
@@ -3,17 +3,21 @@ import { Component, ElementRef, HostListener, Renderer2, ViewChild } from '@angu
 @Component({
   selector: 'about-us',
   templateUrl: './about-us.component.html',
-  styleUrls: ['./about-us.component.css']
+  styleUrls: ['./about-us.component.css', './mobile.css']
 })
 export class AboutUsComponent {
 
   scrolled: number
   finalPosition: number
+  display: string
+  oculto: boolean
 
 
   constructor(private renderer: Renderer2) {
     this.scrolled = 0
     this.finalPosition = -100
+    this.display = "none"
+    this.oculto = false
   }
 
 
@@ -24,6 +28,24 @@ export class AboutUsComponent {
       this.finalPosition = 10;
     }
     else { this.finalPosition = -70 }
+
+  }
+
+  onScrollButton() {
+    this.scrolled = window.scrollY;
+    if (this.scrolled >= 600) {
+      this.display = ""
+    }
+    else { this.display = "none" }
+
+  }
+
+  onScrollRightImage() {
+    this.scrolled = window.scrollY;
+    if (this.scrolled >= 600) {
+      this.oculto = true
+    }
+    else { this.oculto = false }
 
   }
 }
--- src/app/components/main-page/main-page.component.ts
+++ src/app/components/main-page/main-page.component.ts
@@ -3,14 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
-  styleUrls: ['./main-page.component.css']
+  styleUrls: ['./main-page.component.css', './main-page-mobile.css']
 })
 export class MainPageComponent implements OnInit {
 
   video: any
+  scrolled: number
+  oculto: boolean
 
   constructor() {
-
+    this.scrolled = 0
+    this.oculto = false
   }
 
   ngOnInit(): void {
@@ -21,6 +24,13 @@ export class MainPageComponent implements OnInit {
 
   }
 
+  onScrollButton() {
+    this.scrolled = window.scrollY;
+    if (this.scrolled >= 500) {
+      this.oculto = true
+    }
+    else { this.oculto = false }
 
+  }
 
 }
