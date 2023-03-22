8c6bdc13a403ab67b668b4a44dc44bdbc849441b
Oscar Rolón <oscarpcpi2gmai@gmail.com>
2023-03-22 20:51:45 +0100

    copiado main-page a appComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  video: any
+  scrolled: number
+  oculto: boolean
   title = 'front';
+
+  constructor() {
+    this.scrolled = 0
+    this.oculto = false
+  }
+
+  ngOnInit(): void {
+    this.video = document.getElementById('myVideo');
+    this.video.muted = true;
+    this.video.autoplay = true;
+    this.video.loop = true;
+
+  }
+
+  onScrollButton() {
+    this.scrolled = window.scrollY;
+    if (this.scrolled >= 500) {
+      this.oculto = true
+    }
+    else { this.oculto = false }
+
+  }
+
 }
