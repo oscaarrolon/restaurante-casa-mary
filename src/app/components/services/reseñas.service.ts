13f0aeee810a5ce4bd2bb22162007567ae8826f5
Oscar Rolón <oscarpcpi2gmai@gmail.com>
2023-06-08 12:36:59 +0200

    borrado clave api google

--- src/app/components/services/reseñas.service.ts
+++ src/app/components/services/reseñas.service.ts
@@ -11,21 +11,6 @@ export class ReseñasService {
 
   constructor(private http: HttpClient) { }
 
-  obtenerDatos() {
-    this.http.get<any>('https://mybusiness.googleapis.com/v4/accounts/AIzaSyDV2NEOdtLFaeVNpzTTMWkcC6VG1SZrNKQ/locations/ChIJRXGAtUw3Yg0R_UXu9UlphO4/reviews')
-      .subscribe(
-        (data) => {
-          console.log(data); // o haz algo con los datos recibidos
-        },
-        (error) => {
-          console.error(error);
-        }
-      );
-  }
-
-
-
-
 }
 
 
